1f174e690724f4aebfa5d003bc763c5b9e55de43
zhangwuling <zhangwuling@kali.zhangwuling>
2024-08-22 23:29:09 +0700

    update authentication provider

--- src/main/java/com/api/movie/config/WebSecurityConfig.java
+++ src/main/java/com/api/movie/config/WebSecurityConfig.java
@@ -2,6 +2,9 @@ package com.api.movie.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -11,13 +14,28 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import com.api.movie.models.Role;
+import com.api.movie.service.UserService;
 
 import static org.springframework.security.config.Customizer.withDefaults;
+
+import org.springframework.beans.factory.annotation.Autowired;
 
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
+
+    @Autowired
+    @Lazy
+    private UserService userService;
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(new BCryptPasswordEncoder());
+        provider.setUserDetailsService(userService);
+        return provider;
+    }
 
     // Configuration filter chain for authentication
     @Bean
