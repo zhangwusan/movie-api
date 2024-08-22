52c6a9a6e34f79408d81e244d15bc5af7de8d72f
zhangwuling <zhangwuling@kali.zhangwuling>
2024-08-23 03:37:14 +0700

    add autowired

--- src/main/java/com/api/movie/service/MyUserDetailsService.java
+++ src/main/java/com/api/movie/service/MyUserDetailsService.java
@@ -1,5 +1,6 @@
 package com.api.movie.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -12,6 +13,7 @@ import com.api.movie.repositories.UserRepository;
 @Service
 public class MyUserDetailsService implements UserDetailsService {
 
+    @Autowired  
     private UserRepository userRepository;
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
