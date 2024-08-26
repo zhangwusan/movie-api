59c55cd9ea1d960d923f5745d02c8f0e3ad9c177
zhangwuling <zhangwuling@kali.zhangwuling>
2024-08-26 20:36:11 +0700

    add custom handle exception

--- src/main/java/com/api/movie/service/implement/UserServiceImplement.java
+++ src/main/java/com/api/movie/service/implement/UserServiceImplement.java
@@ -9,6 +9,8 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.api.movie.exception.UserAlreadyExistsException;
+import com.api.movie.exception.UserNotFoundException;
 import com.api.movie.models.User;
 import com.api.movie.repositories.UserRepository;
 import com.api.movie.service.JwtService;
@@ -29,11 +31,13 @@ public class UserServiceImplement implements UserService {
     @Autowired
     private final JwtService jwtService;
 
-
-
-
     @Override
     public User createUser(User user) {
+        if (userRepository.findByUsername(user.getUsername()).isPresent()
+                || userRepository.findByEmail(user.getEmail()).isPresent()) {
+            throw new UserAlreadyExistsException(
+                    "User with username " + user.getUsername() + " or email " + user.getEmail() + " already exists.");
+        }
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepository.save(user);
     }
@@ -46,19 +50,19 @@ public class UserServiceImplement implements UserService {
     @Override
     public User getUserByEmail(String email) {
         return userRepository.findByEmail(email)
-                .orElseThrow(() -> new RuntimeException("User does not found by email " + email));
+                .orElseThrow(() -> new UserNotFoundException("User does not found by email " + email));
     }
 
     @Override
     public User getUserById(UUID id) {
         return userRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("User does not found by id " + id.toString()));
+                .orElseThrow(() -> new UserNotFoundException("User does not found by id " + id.toString()));
     }
 
     @Override
     public User getUserByUsername(String username) {
         return userRepository.findByUsername(username)
-                .orElseThrow(() -> new RuntimeException("User does not found by username " + username));
+                .orElseThrow(() -> new UserNotFoundException("User does not found by username " + username));
     }
 
     @Override
@@ -72,7 +76,7 @@ public class UserServiceImplement implements UserService {
             }
             return userRepository.save(user);
 
-        }).orElseThrow(() -> new RuntimeException("User does not found by id " + id.toString()));
+        }).orElseThrow(() -> new UserNotFoundException("User does not found by id " + id.toString()));
     }
 
     @Override
